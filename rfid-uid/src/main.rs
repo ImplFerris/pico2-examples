b195e59502413f3416323bf3273b62da948ca596
ImplFerris <implferris@proton.me>
2024-11-30 09:11:25 +0530

    rfid uid

---
+++ rfid-uid/src/main.rs
@@ -0,0 +1,120 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::delay::DelayNs;
+use hal::block::ImageDef;
+use heapless::String;
+use panic_halt as _;
+use rp235x_hal::{self as hal, Clock};
+
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
+
+use core::fmt::Write;
+
+use embedded_hal_bus::spi::ExclusiveDevice;
+
+use mfrc522::{comm::blocking::spi::SpiInterface, Mfrc522};
+
+use hal::fugit::RateExtU32;
+
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[hal::entry]
+fn main() -> ! {
+    // Boiler Plate
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // For USB Serial
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus);
+
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    // RFID Setup
+    let spi_mosi = pins.gpio7.into_function::<hal::gpio::FunctionSpi>();
+    let spi_miso = pins.gpio4.into_function::<hal::gpio::FunctionSpi>();
+    let spi_sclk = pins.gpio6.into_function::<hal::gpio::FunctionSpi>();
+    let spi_bus = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
+
+    let spi_cs = pins.gpio5.into_push_pull_output();
+    let spi = spi_bus.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000.kHz(),
+        embedded_hal::spi::MODE_0,
+    );
+
+    let spi = ExclusiveDevice::new(spi, spi_cs, timer).unwrap();
+    let itf = SpiInterface::new(spi);
+    let mut mfrc522 = Mfrc522::new(itf).init().unwrap();
+
+    loop {
+        let _ = usb_dev.poll(&mut [&mut serial]);
+        if let Ok(atqa) = mfrc522.reqa() {
+            if let Ok(uid) = mfrc522.select(&atqa) {
+                serial.write("\r\nUID: \r\n".as_bytes()).unwrap();
+                print_hex_to_serial(uid.as_bytes(), &mut serial);
+                timer.delay_ms(500);
+            }
+        }
+    }
+}
+
+fn print_hex_to_serial<B: UsbBus>(data: &[u8], serial: &mut SerialPort<B>) {
+    let mut buff: String<64> = String::new();
+    for &d in data.iter() {
+        write!(buff, "{:02x} ", d).unwrap();
+    }
+    serial.write(buff.as_bytes()).unwrap();
+}
+
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
+    hal::binary_info::rp_cargo_bin_name!(),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"PWM Blinky Example"),
+    hal::binary_info::rp_cargo_homepage_url!(),
+    hal::binary_info::rp_program_build_attribute!(),
+];
