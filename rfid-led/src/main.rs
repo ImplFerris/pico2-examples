1032faf33c138a29ae62f804f7267eb982a75a5b
ImplFerris <implferris@proton.me>
2024-11-30 09:52:20 +0530

    rfid led

---
+++ rfid-led/src/main.rs
@@ -0,0 +1,106 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use hal::block::ImageDef;
+use panic_halt as _;
+use rp235x_hal::{self as hal, Clock};
+
+use hal::fugit::RateExtU32;
+
+use embedded_hal_bus::spi::ExclusiveDevice;
+
+use mfrc522::{comm::blocking::spi::SpiInterface, Mfrc522};
+
+/// Tell the Boot ROM about our application
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[hal::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins up according to their function on this particular board
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    let mut led = pins.gpio25.into_push_pull_output();
+    // RFID Setup
+    let spi_mosi = pins.gpio7.into_function::<hal::gpio::FunctionSpi>();
+    let spi_miso = pins.gpio4.into_function::<hal::gpio::FunctionSpi>();
+    let spi_sclk = pins.gpio6.into_function::<hal::gpio::FunctionSpi>();
+    let spi_bus = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
+
+    let spi_cs = pins.gpio5.into_push_pull_output();
+    let spi = spi_bus.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000.kHz(),
+        embedded_hal::spi::MODE_0,
+    );
+
+    let spi = ExclusiveDevice::new(spi, spi_cs, timer).unwrap();
+    let itf = SpiInterface::new(spi);
+    let mut mfrc522 = Mfrc522::new(itf).init().unwrap();
+
+    // Replace the UID Bytes with your tag UID
+    const TAG_UID: [u8; 4] = [0x13, 0x37, 0x73, 0x31];
+
+    loop {
+        led.set_low().unwrap();
+
+        if let Ok(atqa) = mfrc522.reqa() {
+            if let Ok(uid) = mfrc522.select(&atqa) {
+                if *uid.as_bytes() == TAG_UID {
+                    led.set_high().unwrap();
+                    timer.delay_ms(500);
+                }
+            }
+        }
+    }
+}
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
+    hal::binary_info::rp_cargo_bin_name!(),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"PWM Blinky Example"),
+    hal::binary_info::rp_cargo_homepage_url!(),
+    hal::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
