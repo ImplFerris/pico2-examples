9a0009b4a890e8a59e93cb310b73d91f79ad9605
ImplFerris <implferris@proton.me>
2024-12-01 20:29:42 +0530

    change-auth-key

---
+++ rfid-change-key/src/main.rs
@@ -0,0 +1,190 @@
+#![no_std]
+#![no_main]
+
+use hal::block::ImageDef;
+use heapless::String;
+use panic_halt as _;
+use rp235x_hal::{self as hal, Clock};
+
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
+
+use core::fmt::Write;
+
+use embedded_hal_bus::spi::ExclusiveDevice;
+
+use mfrc522::{comm::blocking::spi::SpiInterface, Mfrc522};
+
+use hal::fugit::RateExtU32;
+
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[hal::entry]
+fn main() -> ! {
+    // Boiler Plate
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // For USB Serial
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus);
+
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    // RFID Setup
+    let spi_mosi = pins.gpio7.into_function::<hal::gpio::FunctionSpi>();
+    let spi_miso = pins.gpio4.into_function::<hal::gpio::FunctionSpi>();
+    let spi_sclk = pins.gpio6.into_function::<hal::gpio::FunctionSpi>();
+    let spi_bus = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
+
+    let spi_cs = pins.gpio5.into_push_pull_output();
+    let spi = spi_bus.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        1_000.kHz(),
+        embedded_hal::spi::MODE_0,
+    );
+
+    let spi = ExclusiveDevice::new(spi, spi_cs, timer).unwrap();
+    let itf = SpiInterface::new(spi);
+    let mut rfid = Mfrc522::new(itf).init().unwrap();
+
+    let target_sector = 1;
+    let rel_block = 3;
+    const DATA: [u8; 16] = [
+        0x52, 0x75, 0x73, 0x74, 0x65, 0x64, // Key A: "Rusted"
+        0xFF, 0x07, 0x80, 0x69, // Access bits and trailer byte
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Key B
+    ];
+    let old_key = &[0xFF; 6];
+    let new_key: &[u8; 6] = &DATA[..6].try_into().unwrap();
+
+    loop {
+        let _ = usb_dev.poll(&mut [&mut serial]);
+
+        if let Ok(atqa) = rfid.reqa() {
+            if let Ok(uid) = rfid.select(&atqa) {
+                serial
+                    .write("\r\n----Before Write----\r\n".as_bytes())
+                    .unwrap();
+                if let Err(e) = read_sector(&uid, target_sector, old_key, &mut rfid, &mut serial) {
+                    serial.write(e.as_bytes()).unwrap();
+                }
+
+                if let Err(e) =
+                    write_block(&uid, target_sector, rel_block, DATA, old_key, &mut rfid)
+                {
+                    serial.write(e.as_bytes()).unwrap();
+                }
+
+                serial
+                    .write("\r\n----After Write----\r\n".as_bytes())
+                    .unwrap();
+                if let Err(e) = read_sector(&uid, target_sector, new_key, &mut rfid, &mut serial) {
+                    serial.write(e.as_bytes()).unwrap();
+                }
+                rfid.hlta().unwrap();
+                rfid.stop_crypto1().unwrap();
+            }
+        }
+    }
+}
+
+fn write_block<E, COMM: mfrc522::comm::Interface<Error = E>>(
+    uid: &mfrc522::Uid,
+    sector: u8,
+    rel_block: u8,
+    data: [u8; 16],
+    key: &[u8; 6],
+    rfid: &mut Mfrc522<COMM, mfrc522::Initialized>,
+) -> Result<(), &'static str> {
+    // const AUTH_KEY: [u8; 6] = [0xFF; 6];
+
+    let block_offset = sector * 4;
+    let abs_block = block_offset + rel_block;
+
+    rfid.mf_authenticate(uid, block_offset, key)
+        .map_err(|_| "Auth failed")?;
+
+    rfid.mf_write(abs_block, data).map_err(|_| "Write failed")?;
+
+    Ok(())
+}
+
+fn read_sector<E, COMM: mfrc522::comm::Interface<Error = E>, B: UsbBus>(
+    uid: &mfrc522::Uid,
+    sector: u8,
+    key: &[u8; 6],
+    rfid: &mut Mfrc522<COMM, mfrc522::Initialized>,
+    serial: &mut SerialPort<B>,
+) -> Result<(), &'static str> {
+    let block_offset = sector * 4;
+    rfid.mf_authenticate(uid, block_offset, key)
+        .map_err(|_| "Auth failed")?;
+
+    for abs_block in block_offset..block_offset + 4 {
+        let data = rfid.mf_read(abs_block).map_err(|_| "Read failed")?;
+        print_hex_to_serial(&data, serial);
+        serial
+            .write("\r\n".as_bytes())
+            .map_err(|_| "Write failed")?;
+    }
+    Ok(())
+}
+
+fn print_hex_to_serial<B: UsbBus>(data: &[u8], serial: &mut SerialPort<B>) {
+    let mut buff: String<64> = String::new();
+    for &d in data.iter() {
+        write!(buff, "{:02x} ", d).unwrap();
+    }
+    serial.write(buff.as_bytes()).unwrap();
+}
+
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
+    hal::binary_info::rp_cargo_bin_name!(),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"RFID Write"),
+    hal::binary_info::rp_cargo_homepage_url!(),
+    hal::binary_info::rp_program_build_attribute!(),
+];
