a4aaac8b844302ad079b22a024f3b474c95993b7
ImplFerris <implferris@proton.me>
2024-12-17 00:09:07 +0530

    joystick send to usb serial

---
+++ joystick-usb/src/main.rs
@@ -0,0 +1,125 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use embedded_hal::{delay::DelayNs, digital::InputPin};
+use embedded_hal_0_2::adc::OneShot;
+use hal::block::ImageDef;
+use heapless::String;
+use panic_halt as _;
+use rp235x_hal as hal;
+
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_serial::SerialPort;
+
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[hal::entry]
+fn main() -> ! {
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    // let mut led = pins.gpio25.into_push_pull_output();
+
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("implRust")
+            .product("Ferris")
+            .serial_number("12345678")])
+        .unwrap()
+        .device_class(2) // 2 for the CDC, from: https://www.usb.org/defined-class-codes
+        .build();
+
+    let mut btn = pins.gpio15.into_pull_up_input();
+
+    let mut adc = hal::Adc::new(pac.ADC, &mut pac.RESETS);
+    let mut adc_pin_0 = hal::adc::AdcPin::new(pins.gpio26).unwrap();
+    let mut adc_pin_1 = hal::adc::AdcPin::new(pins.gpio27).unwrap();
+
+    let mut prev_vrx: u16 = 0;
+    let mut prev_vry: u16 = 0;
+    let mut prev_btn_state = false;
+    let mut buff: String<64> = String::new();
+    let mut print_vals = true;
+    loop {
+        let _ = usb_dev.poll(&mut [&mut serial]);
+
+        let Ok(vrx): Result<u16, _> = adc.read(&mut adc_pin_0) else {
+            continue;
+        };
+        let Ok(vry): Result<u16, _> = adc.read(&mut adc_pin_1) else {
+            continue;
+        };
+
+        if vrx.abs_diff(prev_vrx) > 100 {
+            prev_vrx = vrx;
+            print_vals = true;
+        }
+
+        if vry.abs_diff(prev_vry) > 100 {
+            prev_vry = vry;
+            print_vals = true;
+        }
+
+        let btn_state = btn.is_low().unwrap();
+        if btn_state && !prev_btn_state {
+            let _ = serial.write("Button Pressed\r\n".as_bytes());
+            print_vals = true;
+        }
+        prev_btn_state = btn_state;
+
+        if print_vals {
+            print_vals = false;
+
+            buff.clear();
+            write!(buff, "X: {} Y: {}\r\n", vrx, vry).unwrap();
+            let _ = serial.write(buff.as_bytes());
+        }
+
+        timer.delay_ms(50);
+    }
+}
+
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
+    hal::binary_info::rp_cargo_bin_name!(),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"JoyStick USB"),
+    hal::binary_info::rp_cargo_homepage_url!(),
+    hal::binary_info::rp_program_build_attribute!(),
+];
